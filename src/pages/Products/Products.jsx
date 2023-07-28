46e5f38b5b44ba53b7d8bea889ae8fdd308f3c14
ahmzz <ahmedminhas247@outlook.com>
2023-07-28 12:27:02 +0500

    Products page left side jsx written

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-import './Products.scss'
+import React from "react";
+import "./Products.scss";
 const Products = () => {
   return (
-    <div className='products'>Products</div>
-  )
-}
+    <div className="products">
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="1" value={1} />
+            <label htmlFor="1">Shoes</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="2" value={2} />
+            <label htmlFor="2">Skirts</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id='3' value={3} />
+            <label htmlFor="3">Coats</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filter by price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input type="range" min={0} max={1000} />
+            <span>1000</span>
+          </div>
+          
+        </div>
+        <div className="filterItem">
+          <h2>Sort by</h2>
+          <div className="inputItem">
+            <input type="radio" value='asc' id="asc" name="price" />
+            <label htmlFor="asc">Price (lowest first)</label>
+          </div>
+          <div className="inputItem">
+            <input type="radio" value='desc' id="desc" name="price" />
+            <label htmlFor="desc">Price (highest first)</label>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <img className="catImg" src="https://images.unsplash.com/photo-1506038328654-9c13034fcb54?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default Products
+export default Products;
