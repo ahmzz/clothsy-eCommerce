752414ef7111e63ff1f9253396cf34c6fe061f34
ahmzz <ahmedminhas247@outlook.com>
2023-07-29 19:50:30 +0500

    Left side of products page completed (jsx only)

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Product.scss'
 const Product = () => {
+  const [selectedImg,setSelectedImg]=useState(0)
+  const images=[
+    'https://images.unsplash.com/photo-1535530705774-695729778c55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+    'https://images.unsplash.com/photo-1536588718194-4b38b74a8f54?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'
+  ]
   return (
-    <div className='product' >Product</div>
+    <div className='product' >
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={()=>setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={()=>setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
   )
 }
 
