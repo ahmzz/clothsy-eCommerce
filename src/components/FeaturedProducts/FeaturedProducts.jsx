8bfe4507dd37e29fe4739b3085c6f01d6c532e20
ahmzz <ahmedminhas247@outlook.com>
2023-08-04 17:51:02 +0500

    Strapi server setup, fetching data from strapi and displaying it in jsx

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,41 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./FeaturedProducts.scss";
 import Card from "../Card/Card";
+import axios from "axios";
+import { makeRequest } from "../../makeRequest";
+import useFetch from "../hooks/useFetch";
 const FeaturedProducts = ({ type }) => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.unsplash.com/photo-1625698311031-f0dd15be5144?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
-      title: "Long Sleeve T-shirt",
-      isNew: true,
-      oldPrice: 19,
-      newPrice: 15,
-    },
-    {
-      id: 2,
-      img: "https://images.unsplash.com/photo-1562157873-818bc0726f68?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
-      title: "Polo shirt",
-      isNew: true,
-      oldPrice: 12,
-      newPrice: 10,
-    },
-    {
-      id: 3,
-      img: "https://images.unsplash.com/photo-1523205565295-f8e91625443b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80",
-      title: "Denim Jacket",
-      isNew: true,
-      oldPrice: 12,
-      newPrice: 10,
-    },
-    {
-      id: 4,
-      img: "https://images.unsplash.com/photo-1552374196-1ab2a1c593e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
-      title: "Casual Coat",
-      isNew: false,
-      oldPrice: 12,
-      newPrice: 10,
-    },
-  ];
+  // const data = [
+  //   {
+  //     id: 1,
+  //     img: "https://images.unsplash.com/photo-1625698311031-f0dd15be5144?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+  //     title: "Long Sleeve T-shirt",
+  //     isNew: true,
+  //     oldPrice: 19,
+  //     newPrice: 15,
+  //   },
+  //   {
+  //     id: 2,
+  //     img: "https://images.unsplash.com/photo-1562157873-818bc0726f68?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+  //     title: "Polo shirt",
+  //     isNew: true,
+  //     oldPrice: 12,
+  //     newPrice: 10,
+  //   },
+  //   {
+  //     id: 3,
+  //     img: "https://images.unsplash.com/photo-1523205565295-f8e91625443b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80",
+  //     title: "Denim Jacket",
+  //     isNew: true,
+  //     oldPrice: 12,
+  //     newPrice: 10,
+  //   },
+  //   {
+  //     id: 4,
+  //     img: "https://images.unsplash.com/photo-1552374196-1ab2a1c593e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+  //     title: "Casual Coat",
+  //     isNew: false,
+  //     oldPrice: 12,
+  //     newPrice: 10,
+  //   },
+  // ];
+  const {data,loading,error}=useFetch(`/products?populate=*&[filters][type][$eq]=${type}`)
+
+  
+
   return (
     <div className="featuredProducts">
       <div className="top">
@@ -48,8 +55,8 @@ const FeaturedProducts = ({ type }) => {
         </p>
       </div>
       <div className="bottom">
-        {data.map(item=>(
-            <Card item={item} key={item.id}/>
+        {loading?'loading':data?.map((item) => (
+          <Card item={item} key={item.id} />
         ))}
       </div>
     </div>
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -2,27 +2,24 @@ import React, { useState } from "react";
 import "./Products.scss";
 import List from "../../components/List/List";
 import { useParams } from "react-router-dom";
+import useFetch from "../../components/hooks/useFetch";
 const Products = () => {
   const id = parseInt(useParams().id);
   const [maxPrice, setMaxPrice] = useState(1000);
   const [sort, setSort] = useState(null);
+
+  const {data,loading,error}=useFetch(`/sub-categories?[filters][categories][id][$eq]=${id}`)
+
   return (
     <div className="products">
       <div className="left">
         <div className="filterItem">
           <h2>Product Categories</h2>
-          <div className="inputItem">
+          {data?.map(item=>(<div className="inputItem">
             <input type="checkbox" id="1" value={1} />
-            <label htmlFor="1">Shoes</label>
-          </div>
-          <div className="inputItem">
-            <input type="checkbox" id="2" value={2} />
-            <label htmlFor="2">Skirts</label>
-          </div>
-          <div className="inputItem">
-            <input type="checkbox" id="3" value={3} />
-            <label htmlFor="3">Coats</label>
-          </div>
+            <label htmlFor="1">{item.attributes.title}</label>
+          </div>))}
+          
         </div>
         <div className="filterItem">
           <h2>Filter by price</h2>
